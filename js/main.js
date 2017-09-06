d043f480f6413f00362fecac04c01ef9d56713cc
Peter <ptk726@gmail.com>
2017-09-06 18:09:31 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Jack";
-var cardFour = "Ten";
-console.log("user flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+// console.log("Up and running!");
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree = "queen";
+// var cardFour = "king";
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardTwo);
+
+var cards = ["queen" ,"king", "queen" ,"king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+    result = alert("You found a match!");
+} else {
+	result = alert("Sorry, try again.");
+}
