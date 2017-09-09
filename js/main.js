6633ac78829fb68b53fb6ad4bb874f3fc007f8e0
Peter <ptk726@gmail.com>
2017-09-09 16:51:52 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 
-var cards = ["queen" ,"queen", "king" ,"king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = []; 
 
 var checkForMatch = function() {
@@ -12,9 +34,14 @@ var checkForMatch = function() {
 
 var flipCard = function (cardId) {
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].cardImage);
+
+    cardsInPlay.push(cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].suit);
 
     checkForMatch();
 
@@ -22,5 +49,7 @@ var flipCard = function (cardId) {
 
 flipCard(0);
 flipCard(2);
+
+
 
 
